12d3986ae76d610799d73b50ff56f98f56a0f798
Jeff Johnson <jeff@fpgadeveloper.com>
2015-06-25 19:24:25 -0400

    7z030 part needs 1.8V constraints because it has HP I/O

--- Vivado/build-pz-7z030.tcl
+++ Vivado/build-pz-7z030.tcl
@@ -42,16 +42,16 @@ if {[string equal [get_filesets -quiet constrs_1] ""]} {
 set obj [get_filesets constrs_1]
 
 # Add/Import constrs file and set constrs file properties
-set file "[file normalize "$origin_dir/src/constraints/pzfmc-7z015-7z030.xdc"]"
+set file "[file normalize "$origin_dir/src/constraints/pzfmc-7z015-7z030-1v8.xdc"]"
 set file_added [add_files -norecurse -fileset $obj $file]
-set file "$origin_dir/src/constraints/pzfmc-7z015-7z030.xdc"
+set file "$origin_dir/src/constraints/pzfmc-7z015-7z030-1v8.xdc"
 set file [file normalize $file]
 set file_obj [get_files -of_objects [get_filesets constrs_1] [list "*$file"]]
 set_property "file_type" "XDC" $file_obj
 
 # Set 'constrs_1' fileset properties
 set obj [get_filesets constrs_1]
-set_property "target_constrs_file" "[file normalize "$origin_dir/src/constraints/pzfmc-7z015-7z030.xdc"]" $obj
+set_property "target_constrs_file" "[file normalize "$origin_dir/src/constraints/pzfmc-7z015-7z030-1v8.xdc"]" $obj
 
 # Create 'sim_1' fileset (if not found)
 if {[string equal [get_filesets -quiet sim_1] ""]} {
